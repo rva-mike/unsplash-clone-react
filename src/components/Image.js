a9f3e2254fe0c979f0aca9ab28a36082ad37afc0
rva-mike <mike.levy.rva@gmail.com>
2023-01-10 21:16:52 -0500

    user profile props

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,15 @@
 export default function Image(props) {
     return (
         <>
-        <article className="shadow-md bg-white rounded-3xl p-5">
-            <img src={props.urls.full} alt={props.user.name} loading="lazy" className="h-52 w-full object-cover" />
-        </article>
+            <article className="shadow-md bg-white rounded-3xl p-5">
+                <img src={props.urls.full} alt={props.user.name} loading="lazy" className="h-52 w-full object-cover rounded-3xl md:h-80" />
+
+                <div className="pt-5">
+                    <img src={props.user.profile_image.large} alt={props.user.name} loading="lazy" className="w-20 rounded-full shadow" />
+                </div>
+            </article>
+
+
         </>
     )
 }
