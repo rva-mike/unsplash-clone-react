45fd813c8bfe115913c7f9ae672a359eb519901a
rva-mike <mike.levy.rva@gmail.com>
2023-01-19 21:08:50 -0500

    SearchField basic styling

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -7,7 +7,9 @@ const SearchField = () => {
         type="search" 
         placeholder="search..."
         />
-        <button>Search</button>
+        <button
+        className="bg-blue-600 px-6 py-2.5 text-white rounded-tr rounded-br focus:ring-2 focus:ring-blue-300 disabled:bg-gray-400"
+        >Search</button>
     </div>
   )
 }
