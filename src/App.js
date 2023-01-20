49e01e95c91437d927882806d6e858bbda84acf2
rva-mike <mike.levy.rva@gmail.com>
2023-01-19 20:27:38 -0500

    Jumbotron component start

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import { BrowserRouter, Routes, Route } from "react-router-dom"
 import GetImages from "./components/GetImages"
+import Jumbotron from "./components/Jumbotron"
 
 export default function App() {
   return (
@@ -10,7 +11,7 @@ export default function App() {
       {/* <Route path="/:username" element={<Profile />}></Route> */}
       {/* </Routes> */}
       {/* </BrowserRouter> */}
-
+      <Jumbotron />
       <GetImages />
     </>
   )
---
+++ src/components/Jumbotron.js
@@ -0,0 +1,10 @@
+
+export default function Jumbotron() {
+  return (
+    <div className="bg-gray-900 flex item-center py-10">
+        <div className="max-w-md mx-auto">   
+
+        </div>
+    </div>
+  )
+}
