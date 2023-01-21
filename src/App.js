521c78359b92d4bb036d78ccd593356228849dac
rva-mike <mike.levy.rva@gmail.com>
2023-01-21 14:28:53 -0500

    SearchImages component created

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import GetImages from "./components/GetImages"
 import Jumbotron from "./components/Jumbotron"
 import SearchField from "./components/SearchField"
+import SearchImages from "./components/SearchImages"
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
       <Jumbotron>
         <SearchField/>
       </Jumbotron>
+      <SearchImages />
       <GetImages />
     </>
   )
