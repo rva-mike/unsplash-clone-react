2d401b28423af59d2f5104075ffa370348f40f61
rva-mike <mike.levy.rva@gmail.com>
2023-01-21 19:53:24 -0500

    search image layout, grid

--- src/components/SearchImages.js
+++ src/components/SearchImages.js
@@ -9,7 +9,7 @@ const SearchImages = () => {
     return (
         <>
             <h1 className="text-center mt-6 underline text-2xl">Results for Cats</h1>
-            <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10">
+            <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10 max-w-7xl mx-auto px-4">
                 {response.map((data, key) => <ImageFromSearch key={key} data={data} />)}
             </div>
         </>
