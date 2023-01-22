b5fe78571292adec6455c1c485a14efd9f209250
rva-mike <mike.levy.rva@gmail.com>
2023-01-21 20:53:24 -0500

    image search functional

--- src/components/SearchField.js
+++ src/components/SearchField.js
@@ -1,13 +1,32 @@
+import { useContext, useState } from "react"
+import { ImageContext } from "../App";
 
 const SearchField = () => {
+
+    const [searchValue, setSearchValue] = useState("");
+    const { fetchData } = useContext(ImageContext)
+
+    const handleInputChange = (e) => {
+        setSearchValue(e.target.value)
+    }
+
+    const handleButtonSearch = () => {
+        fetchData(`search/photos?page=1&query=${searchValue}&client_id=${process.env.REACT_APP_UNSPLASH_API_KEY}`)
+        setSearchValue("")
+    }
+
   return (
     <div className="flex">
         <input 
         className="bg-gray-50 border border-gray-300 text-small w-full indent-2 p-2.5 outline-none focus:border-blue-500 focus:ring-2 rounded-tl rounded-bl"
         type="search" 
         placeholder="search..."
+        value={searchValue}
+        onChange={handleInputChange}
         />
         <button
+        onClick={handleButtonSearch}
+        disabled={!searchValue}
         className="bg-blue-600 px-6 py-2.5 text-white rounded-tr rounded-br focus:ring-2 focus:ring-blue-300 disabled:bg-gray-400"
         >Search</button>
     </div>
