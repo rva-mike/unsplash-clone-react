5da3b1ab65f1c9b2027d2d38ffec42ef926d9f0e
rva-mike <mike.levy.rva@gmail.com>
2023-01-21 22:08:37 -0500

    layout changes

--- src/components/SearchImages.js
+++ src/components/SearchImages.js
@@ -9,7 +9,7 @@ const SearchImages = () => {
 
     return (
         <>
-            <h1 className="text-center text-slate-800 font-bold mt-6 underline text-3xl">Results for {searchImage || 'creative'}</h1>
+            <h1 className="text-center text-slate-800 font-bold mt-6  text-4xl md:text-4xl lg:text-5xl">Results for <span className="underline">{searchImage || 'creative'}</span></h1>
             <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 my-10 max-w-7xl mx-auto px-4">
                 {isLoading ? <Skeleton item={10}/> : response.map((data, key) => <ImageFromSearch key={key} data={data} />)}
             </div>
