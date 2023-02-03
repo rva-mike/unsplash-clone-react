0edfe8deba66f74c67fd1304b1338b2e2705a41c
rva-mike <mike.levy.rva@gmail.com>
2023-02-02 21:39:34 -0500

    dropdown navigation and title styles

--- src/components/ExploreTitle.js
+++ src/components/ExploreTitle.js
@@ -1,9 +1,12 @@
+import 'animate.css';
+
+
 
 export const ExploreTitle = () => {
     return (
         <>
             <div id="explore"></div>
-            <h1  className="text-center font-bold text-5xl md:text-5xl lg:text-6xl my-20  lg:mb-16 text-slate-800
+            <h1  className="text-center font-bold text-4xl md:text-5xl lg:text-6xl my-20  lg:mb-16 text-slate-800 mb-0
             ">Explore</h1>
         </>
     )
