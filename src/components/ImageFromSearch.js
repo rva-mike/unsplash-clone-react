f6164d7d2dc2abc2b7c847d1410233520bd649cd
rva-mike <mike.levy.rva@gmail.com>
2023-02-02 22:05:11 -0500

    Deploy React app to GitHub Pages

--- src/components/ImageFromSearch.js
+++
@@ -1,9 +0,0 @@
-const ImageFromSearch = ({ data }) => {
-  return (
-    <a href={data.urls.regular} target="_blank" rel="noreferrer">
-        <img className="h-80 w-full object-cover my-2 mb-3 rounded-lg shadow-md" src={data.urls.small} alt={data.alt_description} />
-    </a>
-  )
-}
-
-export default ImageFromSearch
